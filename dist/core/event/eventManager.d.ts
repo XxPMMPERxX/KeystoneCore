abb97f27169cf0d9ad30354a0ffee49bb67f1a36
gollilla <soradore2@gmail.com>
2025-10-30 09:26:38 +0000

    rm: dist

--- dist/core/event/eventManager.d.ts
+++
@@ -1,7 +0,0 @@
-import { Listener } from './event';
-export declare class EventManager {
-    static instance: EventManager;
-    private listeners;
-    constructor();
-    register(listener: Listener): void;
-}
--- dist/core/keystone.d.ts
+++
@@ -1,7 +0,0 @@
-import { Scheduler } from './scheduler/scheduler';
-declare class Keystone {
-    scheduler: Scheduler;
-    constructor();
-}
-export declare const keystone: Keystone;
-export {};
