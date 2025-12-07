c652aee2d6b5de75bbb0e3bd1a68fb0c70f105d2
VeronicaDiazTaylor <151398472+VeronicaDiazTaylor@users.noreply.github.com>
2025-12-07 09:52:20 +0000

    カスタムアイテムはRPと依存するため、KeystoneCoreから除外

--- core/index.ts
+++ core/index.ts
@@ -2,8 +2,5 @@ export { keystone } from './keystone';
 export { Vector3 } from './math/vector3';
 export { Priority } from './event/priority';
 export { EventManager } from './event/eventManager';
-export { CustomItem } from './item/customItem';
-export { customItemRegistry } from './item/customItemRegistry';
-export { cooldowns } from './item/cooldownManager';
 export { debug } from './utils/debugger';
 export { RepeatingTimer, DelayedTimer, repeating, delayed, sleep } from './timer/timer';
