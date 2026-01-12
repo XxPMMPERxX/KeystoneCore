5b71772554f50e5ac0abe4304ccd262f688a7afe
VeronicaDiazTaylor <151398472+VeronicaDiazTaylor@users.noreply.github.com>
2026-01-12 16:37:46 +0900

    Priorityをtypesに移動

--- core/event/types.ts
+++ core/event/types.ts
@@ -1,7 +1,15 @@
-import { Priority } from "./priority";
-
 /** リスナー型 */
 export interface Listener<T> {
   handler(event: T): void;
   priority?: Priority;
+}
+
+/** 優先度 */
+export enum Priority {
+  LOWEST = 5,
+  LOW = 4,
+  NORMAL = 3,
+  HIGH = 2,
+  HIGHEST = 1,
+  MONITOR = 0
 }
